92ca728a8b02342adcf4fab47a6f537f24029841
SlavaMesh <142238806+SlavaMesh@users.noreply.github.com>
2024-03-15 02:15:21 +0300

    typescript type any to inputRef#4

--- src/pages/technical/index.tsx
+++ src/pages/technical/index.tsx
@@ -2,15 +2,11 @@ import { CityCard } from "../../components/citycard"
 
 
 export const Technical = () => {
-  const getCityList = () => {
-    const list: string | null = localStorage.getItem('cities')
-    return list ? JSON.parse(list) : []
-  }
+
   
   return (
     <div>
-      { getCityList().map((el:any) => <CityCard id={el.id} name={el.name} key={el.id}/>) }
-      
+      Technikal information
     </div>
   )
 }
